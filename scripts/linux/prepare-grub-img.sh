5d2bbd5a853e041aba8b1c51ecd065fe64856424
Rafał Harabień <rafalh1992@o2.pl>
2017-09-21 22:43:55 +0200

    Add GRUB image to easy testing.

--- scripts/linux/prepare-grub-img.sh
+++ scripts/linux/prepare-grub-img.sh
@@ -2,5 +2,7 @@
 SELF_DIR=$(dirname "$0")
 if [ -f "$SELF_DIR/env.sh" ]; then . "$SELF_DIR/env.sh"; fi
 GRUB_DIR="$SELF_DIR/../../grub"
-dd if="$GRUB_DIR/stage1" of="$1" bs=512 count=1
-dd if="$GRUB_DIR/stage2" of="$1" bs=512 seek=1
+#dd if=/dev/zero of="$1" bs=512 count=2880
+#mkdosfs "$1"
+dd if="$GRUB_DIR/stage1" of="$1" bs=512 count=1 conv=notrunc
+dd if="$GRUB_DIR/stage2" of="$1" bs=512 seek=1 conv=notrunc
